3e09f4f877324b3070194f84c650230e8665aee6
Ritvik Saran <RitvikS@teamblr.com>
2023-05-29 15:27:02 +0530

    dp update

--- Csharp/DSA Helper/DSA Helper/Problems/Dynamic Programming/E_GeekJump.cs
+++ Csharp/DSA Helper/DSA Helper/Problems/Dynamic Programming/E_GeekJump.cs
@@ -16,13 +16,26 @@ namespace DSA_Helper.Problems.Dynamic_Programming
         {
             int helper(int n)
             {
-                if (n < 0) return levels[0];
-                return Math.Min(Math.Abs(levels[n] - helper(n - 1)), Math.Abs(levels[n] - helper(n - 2)));
+                if (n == 0) return 0;
+                int left = Math.Abs(levels[n] - levels[n - 1]) + helper(n - 1);
+                int right = (n > 1) ? Math.Abs(levels[n] - levels[n - 2]) + helper(n - 2) : Int32.MaxValue;
+                return Math.Min(left, right);
             }
+            int result = helper(levels.Length - 1);
+            Console.WriteLine(result);
         }
         public void MinimumEnergy_Best()
         {
-
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int i = 1; i < levels.Length; i++)
+            {
+                int left = sum1 + Math.Abs(levels[i] - levels[i - 1]);
+                int right = i > 1 ? sum2 + Math.Abs(levels[i] - levels[i - 2]) : Int32.MaxValue;
+                sum2 = sum1;
+                sum1 = Math.Min(left, right);
+            }
+            Console.WriteLine(sum1);
         }
     }
 }
